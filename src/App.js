295a7f74da4b0d7fc5ebd3f26ae71fcacae85771
hayatemraan <emraanhasanhayat@gmail.com>
2023-07-13 20:29:53 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
+import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
 function App() {
+  useEffect(() => {
+    document.title = "Far away - Exploring";
+  }, []);
   return (
     <div className="App">
       <Header />
       <Form />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+    return <h1 className='footer'>Start adding some items to your packing list 🚀</h1>;
+};
+
+export default Footer;
